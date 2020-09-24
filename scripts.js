04c342dbf2bcf2b9efb74a6e9b0fa30c7928ac45
mikedee95 <70868084+mikedee95@users.noreply.github.com>
2020-09-24 22:18:26 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,8 +12,23 @@ function Hero(image, top, left, size){
     }
 
     this.moveRight = function(){
-        this.left += 60;
+        this.left += 160;
         console.log('ok: ' + this.left);
+    }
+
+    this.moveLeft = function (){
+        this.left -= 160;
+        console.log('ok' + this.left)
+    }
+
+    this.moveDown = function (){
+        this.top += 160;
+        console.log('ok' + this.top)
+    }
+
+    this.moveUp = function (){
+        this.top -= 160;
+        console.log('ok' + this.top)
     }
 
 }
@@ -23,7 +38,11 @@ let hero = new Hero('Tendo.png', 20, 30, 200);
 function start(){
     if(hero.left < window.innerWidth - hero.size){
         hero.moveRight();
-    }
+    } else if(hero.left == window.innerWidth - hero.size){
+        hero.moveDown()
+    } else if(hero.top < window.innerHeight){
+        hero.moveLeft();
+    } else hero.moveRight();
     document.getElementById('game').innerHTML = hero.getHeroElement();
     setTimeout(start, 500)
 }
